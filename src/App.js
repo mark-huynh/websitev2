46a9f0a8d18aa43b2203c9bdecf618db86c4144c
Mark Huynh <mark.huynh@cdk.com>
2019-07-08 19:52:02 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import classNames from 'classnames';
 
 function App() {
+  let imgHorizLine = classNames('imgHorizLine', "col-sm-9");
+  let container = classNames('container', 'row');
+  let imgContainer = classNames('col-sm-6', 'imgContainer');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className = "headera">
+        <h2>
+          Filler Text
+        </h2>
+        <div className = "line"/>
     </div>
-  );
+    <div className={container}>
+      <p className="col-sm-6">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat
+      </p >
+      <div className={imgContainer}>
+        <img className="headshot" src='https://cdn.auth0.com/blog/react-js/react.png' width='70%' height='70%'/>
+      </div >
+    </div>
+  </div>
+);
+
 }
 
 export default App;
