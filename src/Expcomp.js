01156278303882aaaccedfd03bd776fe5b812ed0
Mark Huynh <mark.huynh@cdk.com>
2019-07-09 19:02:29 -0700

    Worked on Experience

---
+++ src/Expcomp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+let styles = {
+  background: "black"
+}
+
+const Expcomp = (props) => {
+  return (
+    <div className = "contentgrid">
+      <div className = "contentCont">
+        <h1>{props.title}</h1>
+        <p>{props.text}</p>
+      </div>
+      <div>
+        <img height = '200px' src = {props.img1}></img>
+        <img height = '200px' src = {props.img2}></img>
+        <img></img>
+      </div>
+    </div>
+);
+
+}
+
+export default Expcomp;
