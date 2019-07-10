7d05446e0b6ce85a0bc37bdc36ee48e8eeefd22e
Bob <asdf>
2019-07-09 19:04:39 -0700

    about me

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import classNames from 'classnames';
 
 function App() {
-  let imgHorizLine = classNames('imgHorizLine', "col-lg-9");
-  let container = classNames('container', 'row');
-  let imgContainer = classNames('col-lg-6', 'imgContainer');
 
   return (
     <div className="main">
+      <div className = "globalContain">
       <div className = "headera">
         <h2>
           <span>Filler Text</span>
@@ -20,22 +17,17 @@ function App() {
 
     <div className = "gridCont">
       <div>
-        text
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat
+        </p >
       </div>
-      <div>
-        image
+      <div className = "imgdiv">
+          <img className="headshot" src='https://cdn.auth0.com/blog/react-js/react.png' width='400' height='400px'/>
       </div>
     </div>
+</div>
+      </div>
 
-    <div className={container}>
-      <p className="col-lg-6">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat
-      </p >
-      <div className={imgContainer}>
-        <img className="headshot" src='https://cdn.auth0.com/blog/react-js/react.png' width='70%' height='70%'/>
-      </div >
-    </div>
-  </div>
 );
 
 }
